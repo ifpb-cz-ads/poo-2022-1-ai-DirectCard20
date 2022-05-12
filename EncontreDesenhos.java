af1bb487dbad197a96279f7f50426c5940ded56e
Guilherme Costa <gui-costaquino@hotmail.com>
2022-05-12 10:50:23 -0300

    Modificando a resenha do primeiro desenho

--- EncontreDesenhos.java
+++ EncontreDesenhos.java
@@ -3,7 +3,7 @@ public class EncontreDesenhos{
         String nomeDesenho = "Cyberchase: A Corrida do Espaço";
         int dataInicio = 2002;
         int quantidadeTemporadas = 13;
-        String resenha = "O desenho mostra com uma leve dose de humor vários cybermistérios com personagens bastante atraentes. Quando o\n         vilão Hacker inventa uma missão para conquistar o universo virtual, a Placa Mãe chama três crianças da Terra \n         para ajudar. Elas são Jackie, Mateus e Inês, os heróis de culturas diferentes de Cyberchase.";
+        String resenha = "O desenho mostra com uma leve dose de humor vários cybermistérios com personagens bastante atraentes. Quando o\n         vilão Hacker inventa uma missão para conquistar o universo virtual, a Placa Mãe chama três crianças da Terra \n         para ajudar. Elas são Jackie, Mateus e Inês, os heróis de culturas diferentes de Cyberchase, que contam com a ajuda\n         do inteligente cyberpássaro Dígito e mergulham no universo do Cyberespaço para acabar com os planos de Hacker.";
         System.out.println("O nome do desenho é: " + nomeDesenho);
         System.out.println("O ano de produção é: " + dataInicio);
         System.out.println("A quantidade de temporadas é: " + quantidadeTemporadas);
