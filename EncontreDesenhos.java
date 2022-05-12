2ccd8a90c23f5370c2b6d3119d559a108cb74ce5
Guilherme Costa <gui-costaquino@hotmail.com>
2022-05-12 10:34:02 -0300

    Implementando e adicionando o programa principlal do projeto

---
+++ EncontreDesenhos.java
@@ -0,0 +1,12 @@
+public class EncontreDesenhos{
+    public static void main(String[] args){
+        String nomeDesenho = "Cyberchase: A Corrida do Espaço";
+        int dataInicio = 2002;
+        int quantidadeTemporadas = 13;
+        String resenha = "O desenho mostra com uma leve dose de humor vários cybermistérios com personagens bastante atraentes. Quando o\n         vilão Hacker inventa uma missão para conquistar o universo virtual, a Placa Mãe chama três crianças da Terra \n         para ajudar. Elas são Jackie, Mateus e Inês, os heróis de culturas diferentes de Cyberchase.";
+        System.out.println("O nome do desenho é: " + nomeDesenho);
+        System.out.println("O ano de produção é: " + dataInicio);
+        System.out.println("A quantidade de temporadas é: " + quantidadeTemporadas);
+        System.out.println("Resenha: " + resenha);
+    }
+}
